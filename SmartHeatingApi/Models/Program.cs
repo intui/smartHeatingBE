6dae040905450a46bbe22f41ef9428416c91b3d8
wwirsam <wirsam@intuisoft.de>
2019-07-07 13:08:03 +0200

    initial commit

---
+++ SmartHeatingApi/Models/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartHeatingApi.Models
+{
+    public class Program
+    {
+        public int ProgramID { get; set; }
+        public string ProgramText { get; set; }
+        public int ProgramType { get; set; }
+        public bool Active { get; set; }
+        public DateTime ValidFrom { get; set; }
+        public DateTime ValidTo { get; set; }
+        public string ProgramContent { get; set; }
+        public int DeviceID { get; set; }
+        public DateTime TSCreated { get; set; }
+        public DateTime TSUpdated { get; set; }
+
+    }
+}
+
+/*
+ * CREATE TABLE [dbo].[Programs](
+	[ProgramID] [int] IDENTITY(1,1) NOT NULL,
+	[ProgramText] [nvarchar](255) NULL,
+	[ProgramType] [int] NULL,
+	[Active] [bit] NULL,
+	[ValidFrom] [date] NULL,
+	[ValidTo] [date] NULL,
+	[Program] [nvarchar](MAX) NULL,
+	[DeviceID] [int] NOT NULL,
+	[TSCreated] [datetime] NOT NULL,
+	[TSUpdated] [datetime] NOT NULL,
+*/
---
+++ SmartHeatingApi/Models/SensorData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartHeatingApi.Models
+{
+    public class SensorData
+    {
+        public int SensorDataID { get; set; }
+        public int SensorID { get; set; }
+        public DateTime TSCreated { get; set; }
+        public int SensorDataType { get; set; } // ToDo: make Enum
+        public string Unit { get; set; }
+        public decimal Value { get; set; }
+        public string ValueText { get; set; }
+    }
+}
+
+/*
+ * create table SensorData (
+	SensorDataID  INTEGER PRIMARY KEY, --Autoincrement in SQLLite
+	SensorID INT not null,
+	TSCreated datetime not null DEFAULT CURRENT_TIMESTAMP,
+	SensorDataType int,
+	Unit nvarchar(50),
+	Value real,
+	ValueText nvarchar(255)
+);
+*/
